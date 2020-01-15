d399cb7d27aeb78442d313da2866325d7bde7a5c
SchmitPSL <schmitpsl@yahoo.com>
2020-01-14 19:09:26 -0500

    A really long message

---
+++ src/FirstProgram.java
@@ -0,0 +1,32 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		int age = 13;
+		double weight = 123.345678;
+		
+		System.out.printf("Hello everyone!%n");
+		System.out.printf("I am %d years old!%n", age);
+		System.out.printf("I weigh %.2f%n", weight);
+		
+		
+		int x = 12;
+		int y = 10;
+		double u = 10.0;
+		System.out.println(x / 0.001);
+		System.out.println(y / u);
+		System.out.println(12.0 % 10.5);
+	}
+
+}
+
+/* 
+	Ways to print:
+	println  with no \n
+	print with \n
+	printf with \n
+	printf with %n
+*/
+/* 
+	+ can be used to split strings
+*/
